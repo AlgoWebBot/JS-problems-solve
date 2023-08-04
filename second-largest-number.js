e612d30e021e051ebd8596f7d7464d47f9b5daea
AlgoWebBot <shimulzahan636@gmail.com>
2023-08-05 01:05:02 +0600

    fibonacci

---
+++ second-largest-number.js
@@ -0,0 +1,18 @@
+
+
+function secondLargestNumber(array) {
+    var largest = arr[0];
+    var index;
+    for (var i = 0; i < array.length; i++){
+        if (array[i] > largest) {
+            
+        }
+    }
+    return index;
+}
+
+
+
+const arr = [5, 10, 15, 7, 6, 11, 13, 19, 16];
+const secondlargestnumber = secondLargestNumber(arr);
+console.log(secondlargestnumber);
---
+++ swap.js
@@ -0,0 +1,8 @@
+var first = 10;
+var second = 15;
+console.log(first, second);
+
+[first, second] = [second, first];
+console.log(first, second);
+
+console.log(Math.floor(16.67));
